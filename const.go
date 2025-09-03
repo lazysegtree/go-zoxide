5a764b16d584479de59cb2dacb0b4cc762e0fcb3
lazysegtree <59679977+lazysegtree@users.noreply.github.com>
2025-09-03 17:05:09 +0530

    feat: Add function to query zoxide and example

---
+++ const.go
@@ -0,0 +1,9 @@
+package zoxide
+
+import "time"
+
+const defaultExecTimeout = 3 * time.Second
+const commandName = "zoxide"
+const noMatchError = "zoxide: no match found"
+const scoreSeperator = " "
+const defaultScore = 0.0
